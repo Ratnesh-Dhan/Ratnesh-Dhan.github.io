ba3e10d1725eeb5b84e64e0b80b280d5dbc21f21
Ratnesh-Dhan <ratneshdhan@gmail.com>
2024-11-21 23:00:01 +0530

    adding in about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -8,11 +8,16 @@ const About = () => {
       <div className='md:m-20 m-5'>
 
       <h2 className='text-6xl font-bold md:text-8xl text-[#697565]'>About.</h2>
-      <p className='font-light text-center md:text-left text-xl mt-5 md:mt-10 text-[#ECDFCC]'>Hi, I&apos;m Ratnesh Dhan, a dedicated software developer with over 1.7 years of experience building dynamic, user-friendly applications. I specialize in front-end development using 
-        <span className='text-[#FFDC7F] font-bold'>Next.js</span> , <span className='text-[#FFDC7F] font-bold'>JavaScript</span> ,<span className='text-[#FFDC7F] font-bold'>TypeScript</span> and <span className='text-[#FFDC7F] font-bold'>JAVA</span>, and I&apos;m always eager to expand my knowledge and skills.
-         I&apos;ve worked on a range of projects, from banking software to freelance web development, where I contributed to delivering seamless user experiences.
+      <p className='font-light text-center md:text-left text-xl mt-5 md:mt-10 text-[#ECDFCC]'>Hi, I&apos;m Ratnesh Dhan, a software developer with 1.8 years of 
+        experience building scalable and user-friendly applications. I specialize in <span className='text-[#FFDC7F] font-bold'>Next.js</span>, <span className='text-[#FFDC7F] font-bold'>React.js</span>, <span className='text-[#FFDC7F] font-bold'>JavaScript</span>, <span className='text-[#FFDC7F] font-bold'>TypeScript</span>, <span className='text-[#FFDC7F] font-bold'>Node.js</span>, <span className='text-[#FFDC7F] font-bold'>Flask</span> and <span className='text-[#FFDC7F] font-bold'>JAVA</span>
+        , delivering efficient and robust solutions across the stack.</p>
 
-I love staying up-to-date with the latest technologies by working on personal projects and exploring new tools like Tailwind CSS and Zustand. While I have dabbled in backend technologies like Node.js and Java, my primary focus is front-end development, where I excel at creating clean, efficient, and responsive code.</p>
+
+<p className='font-light text-center md:text-left text-xl mt-5 md:mt-10 text-[#ECDFCC]'>At LumioAI, I developed a <span className='text-[#FFDC7F] font-bold'>Retrieval-Augmented Generation (RAG)</span> system, 
+leveraging <span className='text-[#FFDC7F] font-bold'>AI/ML</span> technologies to design intelligent query processing and data retrieval workflows, while enhancing front-end interactivity with a user-friendly 
+interface. Previously at Wipro, I contributed to scalable web applications, focusing on state management and responsive design.
+
+</p>
       
       <p className='font-light text-center md:text-left text-xl mt-5 md:mt-10 text-[#ECDFCC]'>         I graduated with a Bachelor of Engineering in Computer Science 
         from <span className='text-[#FFDC7F] font-bold'>Birla Institute of Technology Mesra</span>. During my studies, I gained a solid foundation in programming, algorithms, and software development principles, which have been 
