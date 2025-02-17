3a2c27d097e4e9fd0b84a5458d6469d25e01008a
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-02-17 17:43:28 +0530

    adding new project ImageMod

--- src/components/Projectcard.tsx
+++ src/components/Projectcard.tsx
@@ -1,25 +1,40 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const Projectcard = ({link,  image, title, description}: {link: string, image: string,  title: string, description: string}) => {
+const Projectcard = ({
+  link,
+  image,
+  title,
+  description,
+}: {
+  link: string;
+  image: string;
+  title: string;
+  description: string;
+}) => {
   return (
-    <div className='rounded-lg p-2 mr-5 bg-[#3C3D37]'>
-            <Link href={link} target="_blank" rel="noopener noreferrer" className="block">
-              <Image 
-                src={image}
-                alt={description}
-                width={500} 
-                height={300} 
-                className="rounded-lg"
-              />
-            </Link>
-            <div className='mt-4'>
-            <span className=" font-bold text-[#697565] text-xl">{title}</span>
-            <p className="text-[#ECDFCC] m-2">{description}</p>
-            </div>
+    <div className="rounded-lg p-2 mr-5 bg-[#3C3D37] mb-5 md:mb-10">
+      <Link
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="block"
+      >
+        <Image
+          src={image}
+          alt={description}
+          width={500}
+          height={300}
+          className="rounded-lg mx-auto mt-2"
+        />
+      </Link>
+      <div className="mt-4">
+        <span className=" font-bold text-[#697565] text-xl">{title}</span>
+        <p className="text-[#ECDFCC] m-2 w-[500px]">{description}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projectcard
+export default Projectcard;
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,32 +1,36 @@
-
-import Projectcard from '@/components/Projectcard'
-import React from 'react'
+import Projectcard from "@/components/Projectcard";
+import React from "react";
 
 const Projects = () => {
   return (
     <React.Fragment>
-      <div className='md:m-20 mt-10 ml-4'>
-
-        <h2 className='text-6xl font-bold md:text-8xl text-[#697565]'>Projects.</h2>
-
+      <div className="md:m-20 mt-10 ml-4">
+        <h2 className="text-6xl font-bold md:text-8xl text-[#697565]">
+          Projects.
+        </h2>
       </div>
-      <div className=' flex felx-wrap justify-around md:mx-60 mx-5'>
-        <Projectcard 
+      <div className="flex flex-wrap justify-between items-start md:mx-80 mx-5 ">
+        <Projectcard
+          link="https://github.com/Ratnesh-Dhan/ImageModder/releases/tag/v1.0"
+          image="/ImageMod.png"
+          title="ImageMod"
+          description="An advanced image processing tool for scientific applications, featuring filtering, enhancement, and transformation for precise analysis and visualization."
+        />
+        <Projectcard
           link="https://my-weather-search.vercel.app/"
           image="/WeatherPic.png"
           title="Weather Dashboard"
           description="Weather dashboard app"
         />
-      
-      <Projectcard 
+        <Projectcard
           link="https://vocbuild.com/"
-          image='/Vocbuild.png'
+          image="/Vocbuild.png"
           title="Vocbuild"
           description="Online video dictionary"
         />
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
